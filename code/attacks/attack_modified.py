c8f589f5df2ae8844861c4a7f9d5e37df857d29c
Liorushomir <liorushomir@gmail.com>
2022-08-18 22:26:13 +0300

    Samll change

--- code/attacks/attack_modified.py
+++ code/attacks/attack_modified.py
@@ -71,5 +71,5 @@ class AttackModified(Attack):
         return pert
 
     def optimization_update(self, pert, grad):
-        pert += self.optimizer(grad)
+        pert += self.optimizer.calc_step(grad)
         return pert
